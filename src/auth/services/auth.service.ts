d09a81161555a47f70e6f4eed51a3f7342f5a9b0
zayed <zayed.zaynax02@gmail.com>
2024-09-02 01:21:16 +0600

    microservice is working

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -1,8 +1,7 @@
-import { HttpException, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { ReturnModelType } from '@typegoose/typegoose';
 import { InjectModel } from 'nestjs-typegoose';
-import { RedisMessages } from 'src/modules/microservice-client/messages';
 import { MicroserviceClient } from 'src/modules/microservice-client/microservice-client.module';
 import { CreateUserDto } from '../dto/create-user.dto';
 import { User } from '../entities/user.entity';
@@ -17,10 +16,6 @@ export class AuthService {
 	) {}
 
 	async createUser(body: CreateUserDto): Promise<User> {
-		console.log({ body });
-		const mailSend = await this.redis.send(RedisMessages.COMMUNICATION_SEND_USER_VERIFICATION_MAIL, body);
-		if (!mailSend?.success) throw new HttpException('Can not communicate with communication service', 500);
-		console.log({ mailSend });
-		return await this.userModel.create(body);
+		return body;
 	}
 }
